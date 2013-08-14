608945adf530154676df76356c642ddeacbaf287
Darko Bodnaruk <darko.bodnaruk@gmail.com>
2013-08-14 09:43:05 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bankir1::Application.config.secret_key_base = 'b249c7ba2d3b523b3be7dcbef567723a3b4f8461eb4585e186f01de407f8f6f48e97eb1cf019e741c5121ad8f89d0c6c2ea9e3648de23e9d8c8c5975bb6d53ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bankir1::Application.config.session_store :cookie_store, key: '_bankir1_session'
